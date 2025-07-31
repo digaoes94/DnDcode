be4a261124a410d21a6c8cbf7a758754e1217244
Rodrigo Araujo <rasch.dev@gmail.com>
2025-07-31 16:14:56 -0300

    Project created

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+// don't know yet why i'm doing this
+public class Main {
+	public static Scanner scan = new Scanner(System.in);
+	
+	public static void main(String[] args) {
+
+	}
+
+}
